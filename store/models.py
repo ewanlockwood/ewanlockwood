5b2760a490336dedff953b85619118e592292d13
Ubuntu <ubuntu@ip-172-31-41-238.ec2.internal>
2019-10-08 16:20:34 +0000

    Added Template Model

--- store/models.py
+++ store/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Template(models.Model):
+    title = models.CharField('Template Title', max_length =30)
+    description = models.TextField()
+    image_url = models.CharField('Template Image Url', max_length = 300)
+    color = models.CharField('Template Color', max_length = 10)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+
+    def mean_rating(self):
+        self.reviews.all() # get all ratings
+        
+    def __str__(self):
+        return self.title
+
+        # then compute mean rating and return it
