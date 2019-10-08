393235d58985ed40c29e903ac416cd4162f633f9
Ubuntu <ubuntu@ip-172-31-41-238.ec2.internal>
2019-10-08 17:23:10 +0000

    Created base.html template

--- store/views.py
+++ store/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
 
+from .models import Template
+
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    """ Render index.html """
+    
+    queryset =  Template.objects.all()
+    context = { 'object_list' : queryset,
+                'title': 'List'
+    }
+    
+    return render(request, 'template_page.html', context)
