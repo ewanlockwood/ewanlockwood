d4e26fbc590c02a8d198d51c262421c9529757e2
Ubuntu <ubuntu@ip-172-31-41-238.ec2.internal>
2019-10-18 11:10:02 +0000

    Created one test for Template Model

--- store/tests.py
+++ store/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
+from .models import Template
 
 # Create your tests here.
+
+class ProductTests(TestCase):
+    
+    def test_str(self):
+        test_name = Template(title='Test Template')
+        self.assertEqual(str(test_name), 'Test Template')
