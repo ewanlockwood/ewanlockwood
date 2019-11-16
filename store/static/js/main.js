cf21e7fac71153bd1c7c3a179d0d16dfbca32893
Ubuntu <ubuntu@ip-172-31-41-238.ec2.internal>
2019-11-16 14:18:13 +0000

    added new styling to cart

--- store/static/js/main.js
+++ store/static/js/main.js
@@ -9,3 +9,17 @@ function closeSlideMenu() {
     document.getElementById('navigation-bar').style.marginLeft = '0';
     document.getElementById('menu-toggle').style.display = 'block';
 }
+
+
+// Cart Menu
+function openCartSlideMenu() {
+    document.getElementById('cart-menu').style.width = '375px';
+    document.getElementById('cart-bar').style.marginLeft = '50vw';
+    document.getElementById('menu-toggle').style.display = 'none';
+}
+
+function closeCartSlideMenu() {
+    document.getElementById('cart-menu').style.width = '0';
+    document.getElementById('cart-bar').style.marginLeft = '0';
+    document.getElementById('menu-toggle').style.display = 'block';
+}
