69afe48d8513aaef98b7fd561743c6647e72127b
Ubuntu <ubuntu@ip-172-31-41-238.ec2.internal>
2020-02-18 11:57:38 +0000

    New django forms layout

--- checkout/forms.py
+++ checkout/forms.py
@@ -6,15 +6,6 @@ from crispy_forms.layout import Submit
 
 class MakePaymentForm(forms.Form):
     
-    MONTH_CHOICES = [(i, i) for i in range(1, 12)]
-    YEAR_CHOICES = [(i, i) for i in range(2019, 2030)]
-    
-    credit_card_number = forms.CharField(label='Credit Card Number', required=False)
-    cvv = forms.CharField(label='Security Code (CVV)', required=False)
-    expiry_month = forms.ChoiceField(label='Month', choices=MONTH_CHOICES, required=False)
-    expiry_year = forms.ChoiceField(label='Year', choices=YEAR_CHOICES, required=False)
-    stripe_id = forms.CharField(widget=forms.HiddenInput)
-    
     def __init__(self, *args, **kwargs):
         self.helper = FormHelper()
         self.helper.form_id = 'paymentForm'
@@ -24,6 +15,17 @@ class MakePaymentForm(forms.Form):
 
         self.helper.add_input(Submit('submit', 'Submit'))
         super(MakePaymentForm, self).__init__(*args, **kwargs)
+    
+    MONTH_CHOICES = [(i, i) for i in range(1, 12)]
+    YEAR_CHOICES = [(i, i) for i in range(2019, 2030)]
+    
+    credit_card_number = forms.CharField(label='Credit Card Number', required=False)
+    cvv = forms.CharField(label='Security Code (CVV)', required=False)
+    expiry_month = forms.ChoiceField(label='Month', choices=MONTH_CHOICES, required=False)
+    expiry_year = forms.ChoiceField(label='Year', choices=YEAR_CHOICES, required=False)
+    stripe_id = forms.CharField(widget=forms.HiddenInput)
+    
+    
     
     
 class OrderForm(forms.ModelForm):
