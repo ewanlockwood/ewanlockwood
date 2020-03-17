dbec27fcd92d5be5079d5920dd38ad73bc6a9734
Ubuntu <ubuntu@ip-172-31-41-238.ec2.internal>
2020-03-17 16:06:38 +0000

    Test

--- checkout/views.py
+++ checkout/views.py
@@ -9,12 +9,11 @@ from store.models import Template
 import stripe
 
 # Create your views here.
-
 stripe.api_key = settings.STRIPE_SECRET
 
 @login_required()
 def checkout(request):
-    if request.method == 'POST':
+    if request.method=="POST":
         order_form = OrderForm(request.POST)
         payment_form = MakePaymentForm(request.POST)
         
@@ -22,15 +21,15 @@ def checkout(request):
             order = order_form.save(commit=False)
             order.date = timezone.now()
             order.save()
-
+            
             cart = request.session.get('cart', {})
             total = 0
             for id, quantity in cart.items():
                 template = get_object_or_404(Template, pk=id)
                 total += quantity * template.price
                 order_line_item = OrderLineItem(
-                    order= order,
-                    template = template,
+                    order = order, 
+                    template = template, 
                     quantity = quantity
                     )
                 order_line_item.save()
@@ -38,32 +37,25 @@ def checkout(request):
             try:
                 customer = stripe.Charge.create(
                     amount = int(total * 100),
-                    currency = 'EUR',
+                    currency = "EUR",
                     description = request.user.email,
-                    card = payment_form.cleaned_data['stripe_id']
+                    card = payment_form.cleaned_data['stripe_id'],
                 )
             except stripe.error.CardError:
-                messages.error(request, 'Your card was declined!')
+                messages.error(request, "Your card was declined!")
                 
             if customer.paid:
                 messages.error(request, "You have successfully paid")
                 request.session['cart'] = {}
-                return redirect(reverse('index'))
-                
+                return redirect(reverse('products'))
             else:
-                messages.error(request, 'Unable to take payment')
-                
-                
+                messages.error(request, "Unable to take payment")
         else:
             print(payment_form.errors)
-            messages.error(request, 'We were unable to take your payment')
-            
+            messages.error(request, "We were unable to take a payment with that card!")
     else:
         payment_form = MakePaymentForm()
         order_form = OrderForm()
         
-    return render(request, 'checkout.html', {
-        'order_form': order_form,
-        'payment_form' : payment_form,
-        'publishable' : settings.STRIPE_PUBLISHABLE
-    })
+    return render(request, "checkout.html", {'order_form': order_form, 'payment_form': payment_form, 'publishable': settings.STRIPE_PUBLISHABLE})
+                
