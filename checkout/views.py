5eab21a9c96efaf6d560a2321ebb18d2a7c43b60
Ubuntu <ubuntu@ip-172-31-41-238.ec2.internal>
2020-04-05 18:15:52 +0000

    Removed print statements

--- checkout/views.py
+++ checkout/views.py
@@ -16,8 +16,6 @@ def checkout(request):
     if request.method=="POST":
         order_form = OrderForm(request.POST)
         payment_form = MakePaymentForm(request.POST)
-        print('order: ' + order_form)
-        print('payment: ' + payment_form)
         
         if order_form.is_valid() and payment_form.is_valid():
             order = order_form.save(commit=False)
